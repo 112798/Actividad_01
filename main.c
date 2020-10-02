c93bb7bc7e3975af410a60028619b5d293eccd49
112798 <71934627+112798@users.noreply.github.com>
2020-10-01 22:08:57 -0500

    Agregando programas

---
+++ main.c
@@ -0,0 +1,13 @@
+//Libreria
+#include <stdio.h>
+
+//Funcion principal
+int main() {
+  /*
+  Funcion printf - Ayuda a imptimir
+  o mostrar texto en la pantalla
+  */
+  printf("Hola mundo\n");
+  printf("Este es mi primer programa\n");
+  printf("Soy alumno de nuevo ingreo");
+}
